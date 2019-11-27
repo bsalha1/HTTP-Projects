bae08b45ba0ada16d3dff37a5fa8a8a82c0141b0
= <bsalha1@gmail.com>
2019-11-26 22:29:06 -0500

    Added image loading to http server

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains several projects based on the HTTP protocol
--- basicHttpClientServer/http_server.c
+++ basicHttpClientServer/http_server.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <fcntl.h>
 
+#include <sys/sendfile.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -12,51 +14,57 @@
 #include <unistd.h>
 #include <errno.h>
 #include <signal.h>
-
 #define MAX_SOCKETS 64
 
 void sigHandler(int signal);
 
 int sockets[MAX_SOCKETS] = {0};
 
-int main()
+int main(int argc, char * argv[])
 {
+    if(argc != 3)
+    {
+        exit(EXIT_FAILURE);
+    }
+
     signal(SIGINT, sigHandler);
     char response_data[1024] = {0};
     char http_header[2048] = "HTTP/1.1 200 OK \r\n\n";
     int html_file;
     int server_socket;
     int client_socket;
-    int port = 80;
+    char filename[] = "pages/test.html";
     struct sockaddr_in server_address;
 
     // Open HTML file and read into application
-    html_file = open("pages/index.html", O_RDONLY);
+    html_file = open(filename, O_RDONLY);
     if(html_file == -1)
     {
         perror("File open fail");
         return EXIT_FAILURE;
     }
-    fprintf(stderr, "[OK] index.html opened for reading.\n");
+    fprintf(stderr, "[OK] %s opened for reading.\n", filename);
 
     read(html_file, response_data, sizeof(response_data));
     strcat(http_header, response_data);
 
     // Create socket .. //
+    int optVal = 1;
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == -1)
     {
         perror("Master socket failed to open");
         return EXIT_FAILURE;
     }
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &optVal, sizeof(int));
     fprintf(stderr, "[OK] Master socket %d opened.\n", server_socket);
 
 
 
     // Define address
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(port);
-    server_address.sin_addr.s_addr = ((in_addr_t) 0);
+    server_address.sin_port = htons(atoi(argv[2]));
+    server_address.sin_addr.s_addr = inet_addr(argv[1]);
 
 
 
@@ -84,10 +92,19 @@ int main()
     socklen_t client_addr_len;
     char * client_address;
     int client_port;
+    size_t size_sent;
+
+    char image_header[] = 
+        "HTTP/1.1 200 OK\r\n\n";
+    char imagename[] = "pages/image.jpg";
+    char buffer[2048];
+    struct stat image_stats;
+    int image_file;
+
     while(1)
     {
         // Open client socket ... //
-        client_socket = accept(server_socket, &client_addr, &client_addr_len);
+        client_socket = accept(server_socket, (struct sockaddr *) &client_addr, &client_addr_len);
         if(client_socket == -1)
         {
             fprintf(stderr, "[FAIL] Client socket not accepted: %s\n", strerror(errno));
@@ -97,25 +114,46 @@ int main()
         client_port = ntohs(client_addr.sin_port);
         fprintf(stderr, "[OK] Client %s:%d socket %d accepted.\n", client_address, client_port, client_socket);
 
-
-
-        // Send client socket the header ... //
-        if(send(client_socket, http_header, sizeof(http_header), 0) == -1)
+        if(!fork())
         {
-            fprintf(stderr, "[FAIL] Data failed to send to client socket %d: %s\n", client_socket, strerror(errno));
-            return EXIT_FAILURE;
+            close(server_socket);
+            memset(buffer, 0, 2048);
+            read(client_socket, buffer, 2047);
+
+            printf("Request: %s\n", buffer);
+
+            if(!strncmp(buffer, "GET /image.jpg", 7))
+            {
+                fprintf(stderr, "[OK] Sending client an image\n");
+                write(client_socket, image_header, sizeof(image_header) - 1); // Send header
+
+                image_file = open(imagename, O_RDONLY);
+                if(stat(imagename, &image_stats) == -1)
+                {
+                    perror("Failed to get file size");
+                    exit(EXIT_FAILURE);
+                }
+
+                size_sent = sendfile(client_socket, image_file, NULL, image_stats.st_size); // send file
+                if(size_sent == -1)
+                {
+                    perror("Failed to send file to client");
+                    exit(EXIT_FAILURE);
+                }
+                fprintf(stderr, "[OK] Sent %ld bytes\n", size_sent);
+
+                close(image_file);
+            }
+            else
+            {
+                write(client_socket, http_header, sizeof(http_header) - 1);
+            }
+            close(client_socket);
+            fprintf(stderr, "[OK] Closed connection with client socket %d\n", client_socket);
+            exit(0);
         }
-        fprintf(stderr, "[OK] Data sent to client socket %d.\n", client_socket);
-
-
-
-        // Close client socket ... //
-        if(close(client_socket) == -1)
-        {
-            fprintf(stderr, "[FAIL] Client socket %d failed to close: %s\n", client_socket, strerror(errno));
-            return EXIT_FAILURE;
-        }
-        fprintf(stderr, "[OK] Client socket %d closed.\n", client_socket);
+        
+        close(client_socket);
     }
 
     return EXIT_SUCCESS;
@@ -124,5 +162,6 @@ int main()
 void sigHandler(int signal)
 {
     close(6);
+
     exit(EXIT_SUCCESS);
 }
